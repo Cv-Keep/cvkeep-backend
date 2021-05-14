d1e54648fdeeefa44e37d0906492515dd9038269
Felippe Regazio <felippe.moraes@zoho.com>
2021-05-14 00:18:03 -0300

    added heroku automation scripts

--- sh/heroku-create.sh
+++ sh/heroku-create.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env
 
-# heroku create cvkeep-backend
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-for ITEM in "BASE" "DEFAULT_LANG" "BRAND_NAME" "CLIENT_URL" "SERVER_URL" "UPLOAD_MAX_FILE_SIZE_MB" "JWT_COOKIE_NAME" "REPORT_EMAIL_ADDRESS" "NOREPLY_EMAIL_ADDRESS" "SOCIAL_AUTH_GITHUB_CLIENT_ID" "SOCIAL_AUTH_GITHUB_CLIENT_SECRET" "SOCIAL_AUTH_LINKEDIN_CLIENT_ID" "SOCIAL_AUTH_LINKEDIN_CLIENT_SECRET" "RSA_PUBLIC_KEY" "RSA_PRIVATE_KEY" "APP_SECRET" "MAILER_CONFIG" "MONGODB_URI";
-do
-  echo "Enter value for env key $ITEM:"
-  read VALUE
-  heroku config:set $ITEM="$VALUE"
-done
+heroku create cvkeep-backend
+sh $SCRIPTPATH/heroku-setenv.sh
 
 git push heroku main
 heroku ps:scale web=1
