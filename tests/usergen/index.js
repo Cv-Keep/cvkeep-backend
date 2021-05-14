556111b1f1433c6368344d0e4b10bbe8de58be11
Felippe Regazio <felippe.moraes@zoho.com>
2021-05-14 00:34:36 -0300

    removed database name option from usergen

--- tests/usergen/index.js
+++ tests/usergen/index.js
@@ -1,6 +1,6 @@
 /**
- * This is a CLI script used to generate active cv-keep users on a given mongo database on the current local db.
- * Usage: npm run usergen {db_name} {user_amount}
+ * This is a CLI script is used to generate active cv-keep users on your local database.
+ * Usage: npm run usergen {user_amount}
  * 
  * Every user is loggable with the credentials:
  * 
@@ -8,7 +8,7 @@
  * pass: 'test@test'
  * 
  * If you desire to generate 100.000 users for example, you would run
- * npm run usergen cvkeep 100000
+ * npm run usergen 100000
  * 
  * WARN: This command is destructive, you will lost the database data. 
  * REMENBER: NEVER EVER RUN THIS IN PRODUCTION, NEVER.
@@ -16,18 +16,18 @@
 
 const path = require('path');
 const args = process.argv.slice(2, 4);
-const dn = args[0];
-const qt = args[1];
+
+const qt = args[0];
 const pressAnyKey = require('press-any-key');
 
 global.__basedir = path.resolve('');
 const cvNgrams = require('../../functions/cvNgrams.js');
 
-if (!dn || !qt) {
-  console.log('\nUsage: npm run usergen {db_name} {user_amount}\nWarning: This command will drop the database\n');
+if (!qt) {
+  console.log('\nUsage: npm run usergen {user_amount}\nWarning: This command will drop the local database\n');
   return false;
 } else {
-  console.log(`This is a test script intended to be used on local environments only. The database data will be destroyed. ${qt} active user(s) will be created on the database ${dn}. The loop index will be the user id for email and username. For example, for user 1 the email will be user-1@test.com and,the username will be user-1; For user user-2: user-2@test.com, username user-2. the password will be the same for all users: test@test.`);
+  console.log(`This is a test script intended to be used on local environments only. The database data will be destroyed. ${qt} active user(s) will be created on the local db. The loop index will be the user id for email and username. For example, for user 1 the email will be user-1@test.com and,the username will be user-1; For user user-2: user-2@test.com, username user-2. the password will be the same for all users: test@test.`);
   console.log('');
 }
 
