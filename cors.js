c5b0c56243277f9e5d736c19663b1196200d56d1
Felippe Regazio <felippe.moraes@zoho.com>
2021-05-15 00:43:18 -0300

    exposed cors headers

--- cors.js
+++ cors.js
@@ -23,5 +23,9 @@ module.exports = app => {
 		},
 	}));
 
-	return app;
+	app.use(function(req, res, next) {
+		res.header('Access-Control-Allow-Origin', '*');
+		res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+		next();
+	});
 };
