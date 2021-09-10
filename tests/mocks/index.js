ab5cfe67b4148308ad99149ca4745c7c43cab4bc
Felippe Regazio <felippe.moraes@zoho.com>
2021-09-10 02:02:33 -0300

    removed old tests

--- tests/mocks/index.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  minUser: require('./min-user')
-};
--- tests/mocks/min-user.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  active: true,
-  email: 'john@email.com',
-  username: 'john',
-  fullname: 'John Doe'    
-};
