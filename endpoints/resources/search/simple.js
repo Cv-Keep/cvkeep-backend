a467ce2a7e81c47b536226f65f32acaaa289c760
felippe-regazio <felippe.moraes@zoho.com>
2021-10-21 21:36:50 -0300

    better page management for search

--- endpoints/resources/search/simple.js
+++ endpoints/resources/search/simple.js
@@ -4,18 +4,20 @@ module.exports = async (req, res) => {
 	res.connection.setTimeout(300000);
 
 	const subject = req.query.subject || '';
-	const page = (Number(req.query.page) || 0);
+	const page = Number(req.query.page) || 0;
 	const offset = Number(req.query.offset) || 100;
 
 	const searched = await fnCvNgrams.searchCvByNgrams(subject, page, offset)
 		.catch(error => res.status(500).json({ error, failed: true }));
+
+	console.log(searched);
 
 	res.status(200).json({
 		page,
 		search: subject,
 		items: searched.result,
 		totalItems: searched.count,
-		totalPages: Math.floor(searched.count / offset),
 		isLastPage: page >= Math.floor(searched.count / offset),
+		totalPages: searched.count > offset ? Math.floor(searched.count / offset) : 0,
 	});
 };
