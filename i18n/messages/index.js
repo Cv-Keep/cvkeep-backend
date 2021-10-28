6878dbf18527090e7254559891f480e6b2681e8f
felippe-regazio <felippe.moraes@zoho.com>
2021-10-28 20:30:30 -0300

    french langugae short notation now follows ISO 639-1

--- i18n/messages/index.js
+++ i18n/messages/index.js
@@ -1,9 +1,9 @@
 const en = require('./en.js');
 const ptBr = require('./pt-br.js');
-const frFr = require('./fr-fr.js');
+const fr = require('./fr.js');
 
 module.exports = {
 	'en': en,
 	'pt-br': ptBr,
-	'fr-fr': frFr,
+	'fr': fr,
 };
